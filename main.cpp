7fae15e51a29eeaabb09d7b495c46da36d372dea
KIMIA <kimiabayat16@gnail.com>
2024-01-22 14:37:57 +0330

    renamed easymap.cpp add main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <conio.h>
+
+#include <fstream>
+using namespace std;
+
+
+
+
+
+int main(){
+    // int ch;
+    // ch = getch();
+    // cout << ch;
+    // S=115;
+    //W=119;
+    //a=97;
+    //d=100;
+    int number;
+    cout<<"1. create a new map"<<endl<<"2. playground"<<endl<<"3. Solve a maze"<<endl<<"4. History"<<endl<<"5. leaderboard"<<endl<<"6. Exit"<<endl;
+    system("cls");
+    cin>>number;
+    switch(number){
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:
+        break;
+    }
+}
