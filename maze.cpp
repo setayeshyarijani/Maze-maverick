f7c5b8d84e3262df15ca11b8a4f03603217e5a6f
Setayesh yarijani <152412073+setayeshyarijani@users.noreply.github.com>
2024-01-27 00:25:34 +0330

    findpath continue

--- maze.cpp
+++ maze.cpp
@@ -52,3 +52,75 @@ void createmap()
         max_zero = min(max_zero , m * n - step - 1);
     }
 
+    bool pathfound=false;
+    vector<vector<int>> a(n, vector<int>(m));
+        for(int i = 0; i < n; i++)
+          {
+            for(int j = 0; j < m; j++)
+             {
+                a[i][j] = 2;
+             }
+          }
+    int pathLength = 0;
+    int flag = 1;
+    findPath(0 , 0 , n, m, pathLength, step ,a , flag);
+    random_device rd;
+    mt19937 gen(rd());
+    int num_zeros = min_zero + rand() % (max_zero - min_zero + 1);
+    vector<vector<int>> backup_a = a;
+    bool validMatrix = true;
+    int sum = 0;
+    int zeros_placed = 0;
+    do {
+        validMatrix = true;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) 
+             {
+                if (zeros_placed < num_zeros && rand()% max_zero == 0 && (a[i][j] != 1) && (a[i][j] != 0)) 
+                 {
+                    a[i][j] = 0;
+                    zeros_placed++;
+                 }
+                if (zeros_placed != num_zeros) 
+                {
+                    validMatrix = false;
+                }
+            }
+    } 
+    while (!validMatrix);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++) 
+        {
+            if((a[i][j] == 1) && !(i == n - 1 && j == m - 1))
+            {
+                do
+                 {
+                    a[i][j] = rand() % (max_num - min_num + 1) + min_num;
+                 }
+                while (a[i][j] == 0);
+                sum += a[i][j];
+            }
+            else if(a[i][j] != 0)
+            {
+               do
+                {
+                  a[i][j] = rand() % (max_num - min_num + 1) + min_num;
+                }
+               while (a[i][j] == 0);
+            }
+        }
+    a[n - 1][m - 1] = sum;
+    for (int i = 0; i < n; i++) 
+    {
+            for (int j = 0; j < m; j++) 
+              {
+
+                cout << left << setw(4) << to_string(a[i][j]) << " ";
+              }
+            cout << endl;
+    }
+    savemap(n, m, step, a);
+    cout << "press enter to continue";
+    getch();
+}
+
